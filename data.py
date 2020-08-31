ac84430002d8916c5c0c93ac51193f74b7c032d8
abdallah197 <abashir@ceres.mpi-inf.mpg.de>
2020-08-31 19:50:28 +0200

    added mlp and main model and loss and fixed config to lm_config

--- data.py
+++ data.py
@@ -5,24 +5,25 @@ from transformers import AutoTokenizer
 import pandas as pd
 
 
-from config import *
+from config_lm import *
 
 
 class DoubleSynonymsDataset(Dataset):
 
     """ Create a datset for the boylm model """
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, mode):
         self.csv_file = pd.read_csv(csv_file)
-        self.tokenizer = AutoTokenizer.from_pretrained(config.model)
-
-        self.tokenized_two_tagets = self.tokenzing(self.csv_file)
+        self.tokenizer = AutoTokenizer.from_pretrained(config_lm.model)
+        self.mode = 1 if mode == "source" else 0
+        self.csv_file = self.new_masked_columns(self.csv_file)
+        self.tokenized_text = self.tokenzing(self.csv_file, self.mode)
 
     def __len__(self):
-        return len(self.tokenized_two_tagets)
+        return len(self.tokenized_text)
 
     def __getitem__(self, idx):
-        return self.tokenized_two_tagets[idx]
+        return self.tokenized_text[idx]
 
     def mask_target(self, sentence):
         sentence = sentence.split()
@@ -30,22 +31,23 @@ class DoubleSynonymsDataset(Dataset):
         masked_index = random.randrange(0, length, 1)
         sentence[masked_index] = self.tokenizer.mask_token
 
-        return "".join(sentence)
+        return " ".join(sentence)
 
     def new_masked_columns(self, file):
         file["target1_masked"] = file["target1"].apply(self.mask_target)
         file["target2_masked"] = file["target2"].apply(self.mask_target)
+
         file["text_masked"] = file.apply(
             lambda x: [
-                [x["source"] + " " + x["rela"] + " " + x["target1_masked"]],
-                [x["source"] + " " + x["rela"] + " " + x["target2_masked"]],
-            ],
-            axis=1,
+                [x["source"] + " " + x["rela"] + " " + x[f"target1_masked"]],
+                [x["source"] + " " + x["rela"] + " " + x[f"target2_masked"]]
+            ]
+               , axis=1
         )
 
         return file
 
-    def tokenzing(self, file):
+    def tokenzing(self, file, mode):
         """
         We need labels1 for the unmasked text 1 since the model will do the computation on the labels 1
 
@@ -54,7 +56,7 @@ class DoubleSynonymsDataset(Dataset):
         text_unmasked = list(zip(*file["text"].tolist()))
         text_unmasked_1, text_unmasked_2 = text_unmasked[0], text_unmasked[1]
 
-        text_masked = file["text_masked"].tolist()
+        text_masked = list(zip(*file["text_masked"].tolist()))
         text_masked_1, text_masked_2 = text_masked[0], text_masked[1]
 
         text_target_1_unmasked = self.tokenizer(
@@ -70,12 +72,17 @@ class DoubleSynonymsDataset(Dataset):
         text_target_2_masked = self.tokenizer(
             text_masked_2, padding=True, truncation=True, return_tensors="pt"
         )
-
-        return list(
-            zip(
-                text_target_1_unmasked,
-                text_target_2_unmasked,
-                text_target_1_masked,
-                text_target_2_masked,
+        if mode == 1:
+            return list(
+                zip(
+                    text_target_1_unmasked,
+                    text_target_1_masked                )
             )
-        )
+        else:
+            return list(
+                zip(
+                    text_target_2_unmasked,
+                    text_target_2_masked
+                )
+            )
+
--- model.py
+++ model.py
@@ -1,18 +1,95 @@
-from config import params
-from transformers import AutoModel
+from transformers import AutoModel, AlbertPreTrainedModel, AlbertModel
+from transformers.modeling_albert import AlbertMLMHead
+
 from torch import nn
+import torch.nn.functional as F
 
 
-class RobertaBOYL(nn.Module):
-    def __init__(self):
-        super(BertTripletClassifier, self).__init__()
-        self.model = AutoModel.from_pretrained(params["model"])
-        self.dropout = nn.Dropout(0.25)
-        self.out = nn.Linear(768, 1)
+from config_lm import *
 
-    def forward(self, ids, mask, token_type_ids):
-        _, cls = self.model(ids, attention_mask=mask, token_type_ids=token_type_ids)
 
-        bert_out = self.dropout(cls)
-        output = self.out(bert_out)
-        return output
+def boyl_loss(x, y):
+    """
+    minimize the loss function 
+    """
+    x = F.normalize(x, dim=-1, p=2)
+    y = F.normalize(y, dim=-1, p=2)
+    return 2 - 2 * (x * y).sum(dim=-1)
+
+class MLPHead(nn.Module):
+    def __init__(self, in_channels, mlp_hidden_size, projection_size):
+        super(MLPHead, self).__init__()
+        
+        self.net = nn.Sequential(
+            nn.Linear(in_channels, mlp_hidden_size),
+            nn.BatchNorm1d(mlp_hidden_size),
+            nn.ReLU(inplace=True),
+            nn.Linear(mlp_hidden_size, projection_size)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+"""
+add mlp_hidden_size, projection_size to config
+"""
+class AlbertBOYL(AlbertPreTrainedModel):
+    def __init__(self, config, mlp_hidden_size, projection_size):
+        super().__init__(config)
+
+        self.albert = AlbertModel(config)
+        self.predictions = AlbertMLMHead(config)
+        self.mlp =  MLPHead(config.hidden_size, mlp_hidden_size, projection_size)
+
+        self.init_weights()
+        self.tie_weights()
+
+    def tie_weights(self):
+        self._tie_or_clone_weights(self.predictions.decoder, self.albert.embeddings.word_embeddings)
+
+    def get_output_embeddings(self):
+        return self.predictions.decoder
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        **kwargs
+    ):
+
+        outputs = self.albert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+        sequence_outputs = outputs[0]
+
+        prediction_scores = self.predictions(sequence_outputs)
+        # make sure about the prediction_scores dim
+        prediction_scores = self.mlp(prediction_scores)
+
+        outputs = (prediction_scores,) + outputs[2:]  # Add hidden states and attention if they are here
+        if labels is not None:
+            boyl_lm_loss = boyl_loss(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
+            outputs = (boyl_lm_loss,) + outputs
+
+        return outputs
+
+
+"""
+TODO:
+1. implement any thing class by class and try in a notebook.
+2. initiliaze in a notebook with one example.
+"""
